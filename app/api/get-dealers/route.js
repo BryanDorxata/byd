e60b6c1763106bb8c3b892c2d990e3e21e7f5ef0
BryanDorxata <133320281+BryanDorxata@users.noreply.github.com>
2025-07-17 22:14:34 +0800

    Update route.js

--- app/api/get-dealers/route.js
+++ app/api/get-dealers/route.js
@@ -1,18 +1,21 @@
-export default defineEventHandler(async () => {
+export async function GET() {
   const WEBFLOW_TOKEN = process.env.WEBFLOW_TOKEN;
 
-  const res = await fetch("https://api.webflow.com/v2/collections/67a328e1c604a981c58db5b3/items?offset=0&limit=1001", {
-    headers: {
-      Authorization: `Bearer ${WEBFLOW_TOKEN}`,
-      "Content-Type": "application/json",
-      "accept-version": "2.0.0"
-    },
-  });
+  const res = await fetch(
+    'https://api.webflow.com/v2/collections/67a328e1c604a981c58db5b3/items?offset=0&limit=1001',
+    {
+      headers: {
+        Authorization: `Bearer ${WEBFLOW_TOKEN}`,
+        'Content-Type': 'application/json',
+        'accept-version': '2.0.0',
+      },
+    }
+  );
 
   const json = await res.json();
 
-  // Extract only the names from the fieldData
-  const names = json.items.map(item => item.fieldData.name);
+  // Extract the dealer names
+  const names = json.items.map((item) => item.fieldData.name);
 
-  return names;
-});
+  return Response.json(names);
+}
